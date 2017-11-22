95d8e0d4d75933fcdfacb6429555f8ac75ee599e
fanxinchen <40666638@qq.com>
2017-11-22 16:07:43 +0800

    Assets删除

--- JungleWarsClient/Assets/Scripts/Player/PlayerInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Common;
-public class PlayerInfo : MonoBehaviour {
-    public RoleType roleType;
-    public bool isLocal = false;
-}
